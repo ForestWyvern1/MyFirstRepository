0f885503f4d1a05e0159d50764104ab61f4f122d
Vladimir <familkas8@gmail.com>
2022-04-21 17:39:19 +0300

    FirstCommit

---
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.firstproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
